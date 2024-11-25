3c7faedb9c713a14c5bbf8d3a0fd695033a3e3dc
GM110Z <72254136+GM110Z@users.noreply.github.com>
2024-11-25 20:37:36 +0000

    Update calculate-size.sh

--- calculate-size.sh
+++ calculate-size.sh
@@ -3,6 +3,9 @@ echo "Downloading and measuring genomes"
 #download genomes where hits were found
 parallel -a $1 -C '\t' -j8 --delay 1.5 "efetch -db nuccore -id {1} -format fasta >{1}.fasta"  
 
+#edit headers to accomodate faids
+sed -i.bak 's/>\(.*\):.*$/>\1/' *.fasta
+sed -i.bak 's/>\(.*\):[0-9]*-[0-9]*/>\1/' *.fasta
 
 #get sizes of each chromosome with faidx
 VAR2=$(echo *.fasta | xargs ls)
